209f4e3676802a9d304b5f4c6ea86bb97b023445
Adam Greenhall <adam.greenhall@gmail.com>
2013-09-13 21:26:37 -0700

    get trips, stops, with real-time updates working

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, request
 import pandas as pd
 from config import con, config, service_type
 import subprocess
+from datetime import timedelta
+
 from ipdb import set_trace
 
 app = Flask(__name__)
@@ -27,65 +29,80 @@ def create_schedule(date, route_id):
     # take the params and create a properly formatted schedule df
     date = pd.Timestamp(date)
     # first get the trips and stops for the route
-    query = """
-    select 
-        t.trip_id, t.direction_id, 
-        st.stop_id, st.stop_sequence, st.arrival_time, st.departure_time,
-        stu.arrival_delay, stu.departure_delay
-    from 
-        trips t, stop_times st, 
-        trip_updates tu, stop_time_updates stu
-    where 
-        t.trip_id = st.trip_id and t.trip_id = tu.trip_id and
-        tu.oid = stu.trip_update_id and 
-        stu.stop_id = st.stop_id    
+    query_schedule = """
+      select 
+          trips.trip_id, trips.direction_id, 
+          st.stop_id, st.stop_sequence, st.arrival_time, st.departure_time
+      from trips, stop_times st
+      where trips.trip_id = st.trip_id    
+    """
+    query_updates = """
+        select 
+            tu.trip_id, tu.timestamp,
+            stu.stop_id, stu.arrival_delay, stu.departure_delay
+          from trip_updates tu, stop_time_updates stu
+          where tu.oid = stu.trip_update_id
     """
     
-    query += """
-        and t.service_id='{service}' 
-        and t.route_id=%s""".format(service=service_type(date))
-    # query += " and tu.timestamp between '{d}' and '{nd}'".format(
-    #     d=date.date(), nd=(date + pd.DateOffset(days=1)).date())        
+    query_schedule += """
+        and trips.service_id='{service}' 
+        and trips.route_id=%s""".format(service=service_type(date))
+    query_updates += " and tu.timestamp between '{d}' and '{nd}'".format(
+        d=date.date(), nd=(date + pd.DateOffset(days=1)).date())
+    
+    query = """
+    select 
+        s.trip_id, s.direction_id,
+        s.stop_id, s.stop_sequence, s.arrival_time, s.departure_time,
+        u.arrival_delay, u.departure_delay, u.timestamp
+    from ({s}) s   left join ({u}) u
+    on (u.trip_id = s.trip_id and u.stop_id = s.stop_id)
+    order by s.trip_id, s.stop_sequence
+    """.format(s=query_schedule, u=query_updates)
+    
     df = pd.read_sql(query, con, params=[route_id])
     
-    
     trips = []
+    trip_stops = {}
     for (id_trip, trip_direction), stops in \
         df.groupby(['trip_id', 'direction_id']):
+        stops['time_arrival'] = pd.to_datetime(
+            str(date.date()) + ' ' + stops.arrival_time
+            ) #  + stops.departure_delay.fillna(0).shift(1)
+
+        if (stops.departure_delay > 0).any():
+            # departure delay = arrival delay at next stop
+            stops['time_arrival'] += stops.departure_delay.shift(1).fillna(0)\
+                .apply(lambda t: timedelta(seconds=t))
         
-        set_trace()
+        col_names=dict(
+            stop_id='id_stop',
+            time_arrival='time_arrival',
+            )
+        stops = stops[col_names.keys()].rename(columns=col_names)
+        stops['count'] = 0
+        stops['count_boarding'] = 0
+        stops['count_exiting'] = 0
+        
+        trip_stops[id_trip] = stops.to_json(orient='records')
         trips.append(dict(
             id_trip=id_trip,
             trip_direction=trip_direction,
-            stops=stops,
+            stops="__trip_stops__{}".format(id_trip),
             ))
-    
-    # "trips": [
-    #     {
-    #         "id_trip": "758",
-    #         "trip_direction": 1,
-    #         "stops": [
-    #             {
-    #                 "count": 0,
-    #                 "time_arrival": 1349077922,
-    #                 "id_stop": 3893,
-    #                 "count_boarding": 0,
-    #                 "count_exiting": 0
-    #             }, ...
-    #         ]}, 
-    # ... ]
 
+    
     data = dict(
         date=str(date.date()),
         stop_locations={}, # list of dict(distance, direction, id_stop)
         id_route=route_id,
         trips=trips,
         )
-        
     
     response = jsonify(data=data)
-    # response.data = response.data.replace(
-    #     '"{}"'.format(placeholder), df.to_json(orient='records'))
+    for id_trip, stops_json in trip_stops.iteritems():
+        response.data = response.data.replace(
+            '"__trip_stops__{}"'.format(id_trip), stops_json)
     return response
 
 @app.route('/test-db')
